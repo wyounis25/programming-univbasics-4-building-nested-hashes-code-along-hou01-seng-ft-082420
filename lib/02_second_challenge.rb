525b84f1b446b139f21f3711eceb0eafc6e50a31
walid younis <walid.younis.90@gmail.com>
2020-08-20 21:18:28 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
-      :hero_friends = []
-      }, 
-    :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
-      :heroine_friends = []
-      
-      }}
+ epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 
   
 end
