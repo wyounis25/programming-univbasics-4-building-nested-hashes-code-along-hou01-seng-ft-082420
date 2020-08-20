0aca9284d8deae78fe4fb362e93304ffa1e206da
walid younis <walid.younis.90@gmail.com>
2020-08-20 21:18:14 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,9 +1,8 @@
 def third_challenge
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
-      :patriarch => {
-       :name => "Lord Montague", :age => "53"},
-       :matriarch => {name: "Lady Montague", age: "54"},
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
@@ -13,7 +12,6 @@ def third_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
-  }
-
+}
   
 end
